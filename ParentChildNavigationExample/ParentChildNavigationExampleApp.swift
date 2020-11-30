d9da2eda3bf11a282fcd76e0bbf2e18d8213d2ff
Russell Gordon <rgordon@lcs.on.ca>
2020-11-30 18:45:05 -0500

    Initial Commit

---
+++ ParentChildNavigationExample/ParentChildNavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParentChildNavigationExampleApp.swift
+//  ParentChildNavigationExample
+//
+//  Created by Russell Gordon on 2020-11-30.
+//
+
+import SwiftUI
+
+@main
+struct ParentChildNavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
